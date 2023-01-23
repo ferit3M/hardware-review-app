0eed586d288d582dca6f2ad7b09d324298c7f5ed
tmarkovica.erit <tomislav@erit.hr>
2023-01-23 18:45:41 +0100

    Returning user id

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -12,22 +12,38 @@ export class UserService {
   private API_URL = environment.API_URL;
 
   private ACCESS_TOKEN_STORAGE_KEY = 'access_token';
+  private ACCESS_USER_ID_KEY = 'user_id';
+  private ACCESS_USER_NAME_KEY = 'user_name';
+  private ACCESS_USER_EMAIL_KEY = 'user_email';
 
   readonly loggedin: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   private access_token: string = null;
+  private id: number;
+  private name: BehaviorSubject<string> = new BehaviorSubject('');
+  private email: string;
 
   constructor(
     private http: HttpClient,
   ) {
     this.access_token = localStorage.getItem(this.ACCESS_TOKEN_STORAGE_KEY);
-    if (this.access_token)
-      this.loginSuccesful(this.access_token);
+    this.id = Number(localStorage.getItem(this.ACCESS_USER_ID_KEY));
+    this.name.next(localStorage.getItem(this.ACCESS_USER_NAME_KEY));
+    this.email = localStorage.getItem(this.ACCESS_USER_EMAIL_KEY);
+
+    if (this.access_token && this.id !== 0 && this.name.getValue() && this.email)
+      this.loggedin.next(true);
   }
 
-  private loginSuccesful(access_token: string) {
+  private loginSuccesful(access_token: string, id: number, name: string, email: string) {
     localStorage.setItem(this.ACCESS_TOKEN_STORAGE_KEY, access_token);
+    localStorage.setItem(this.ACCESS_USER_ID_KEY, id.toString());
+    localStorage.setItem(this.ACCESS_USER_NAME_KEY, name);
+    localStorage.setItem(this.ACCESS_USER_EMAIL_KEY, email);
     this.loggedin.next(true);
+    this.id = id;
+    this.name.next(name);
+    this.email = email;
   }
 
   login(body: UserLogin): Promise<boolean> {
@@ -35,7 +51,7 @@ export class UserService {
       console.log(res);
 
       if (res.access_token) {
-        this.loginSuccesful(res.access_token);
+        this.loginSuccesful(res.access_token, res.user.id, res.user.name, res.user.email);
         return true;
       }
     }).catch(err => {return false});
@@ -46,7 +62,7 @@ export class UserService {
       console.log(res);
 
       if (res.access_token) {
-        this.loginSuccesful(res.access_token);
+        this.loginSuccesful(res.access_token, res.user.id, res.user.name, res.user.email);
         return true;
       }
     }).catch(err => { return false; });
@@ -64,5 +80,13 @@ export class UserService {
       })
     };
     return options;
+  }
+
+  getUserId(): number {
+    return this.id;
+  }
+
+  getEmail(): string {
+    return this.email;
   }
 }
